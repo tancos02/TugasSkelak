524c8cfe960ba7442846ec0a1156069bd0e5184e
Paulus <37735433+tancos02@users.noreply.github.com>
2021-12-16 01:42:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,23 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+import numpy as np
+import altair as alt
 
-df
+nation_name = df_js['name']
+
+with st.container():
+    option = st.selectbox(
+        "Nation",
+        nation_name
+    )
+    choosen_nation = df_js.loc[df_js['name'] == option]
+    choosen_nation.reset_index(drop=True, inplace=True)
+    nation_code = str(choosen_nation['alpha-3'][0])
+    result = df.loc[df['kode_negara'] == nation_code]
+    result = result[['tahun', 'produksi']]
+    result.reset_index(drop=True, inplace=True)
+    chart = alt.Chart(result).mark_line().encode(
+        x='tahun',
+        y='produksi'
+    )
+    st.altair_chart(chart, use_container_width=True)
